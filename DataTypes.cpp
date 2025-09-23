d32672cf79a875c8f71ae45f4c0880b4ddfced78
YAKANKSHAKESAVAPATNAM <yakankshakesavapatnam@gmail.com>
2025-09-23 20:28:06 +0530

    DataTypes

---
+++ DataTypes.cpp
@@ -0,0 +1,15 @@
+//Data Types in c++
+//#include<iostream> OR 
+#include <bits/stdc++.h>
+
+using namespace std;
+int main(){
+	int a=10;
+	cout<<a;
+	return 0;
+}
+
+//CLASSIFICATION OF DATATYPES
+//1->BASIC DATATYPES-->int,float,double,char,bool,void
+//2->Derived DATATYPES-->array,pointer,refernce,function
+//3->User Defined DataTypes-->class,structure,union,typedef,using
