df8759f3533871bfc0f0fefcb111e28c82f840e3
YAKANKSHAKESAVAPATNAM <yakankshakesavapatnam@gmail.com>
2025-09-23 18:20:12 +0530

    basic program

---
+++ Basic_syntax.cpp
@@ -0,0 +1,15 @@
+// Necessary header files for input output functions
+#include <iostream>
+//#include <bits/stdc++.h>
+using namespace std;
+
+// main() function: where the execution of
+// C++ program begins
+int main() {
+  
+    // This statement prints "Hello World"
+    cout << "Hello World";
+    //std::cout<< "hello world";
+
+    return 0;
+}
