7b04378872dc5f675710ced50151a2c5962a1af2
YAKANKSHAKESAVAPATNAM <yakankshakesavapatnam@gmail.com>
2025-12-29 21:59:39 +0530

    remove duplicates from sorted array

--- RemoveDuplicaatedSoretedArray.cpp
+++ RemoveDuplicaatedSoretedArray.cpp
@@ -15,4 +15,18 @@ int removeDuplicates (vector<int> &arr, int){
 		}
 	}
 	return i+1;
-}
+};
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i=0;
+        for(int j=1;j<nums.size();j++){
+            if(nums[i]!=nums[j]){
+                nums[i+1]=nums[j];
+                i++;
+            }
+        }     
+        return i+1;
+    }
+};
