20547d252fdc6d39258fe92fda4f7f274b97bff8
YAKANKSHAKESAVAPATNAM <yakankshakesavapatnam@gmail.com>
2025-12-30 20:41:41 +0530

    SecondLargest in Array

--- SecondLargestInArray.cpp
+++ SecondLargestInArray.cpp
@@ -14,32 +14,23 @@
 //		return slargest;
 //	}
 
-#include <iostream>
-#include <vector>
-using namespace std;
-
 class Solution {
-public:
-    int secondlargest(vector<int> &arr, int n) {
-        int largest = arr[0];
-        int slargest = -1;
-
-        for (int i = 1; i < n; i++) {
-            if (arr[i] > largest) {
-                slargest = largest;
-                largest = arr[i];
+  public:
+    int getSecondLargest(vector<int> &arr) {
+        // code here
+        int largest=arr[0];
+        int slargest=-1;
+        for (int i=1;i<arr.size();i++){
+            if(arr[i]>largest){
+                slargest=largest;
+                largest=arr[i];
             }
-            else if (arr[i] < largest && arr[i] > slargest) {
-                slargest = arr[i];
+            else if(arr[i]<largest && arr[i]>slargest){
+                slargest=arr[i];
             }
         }
         return slargest;
     }
 };
 
-int main() {
-    vector<int> arr = {10, 20, 49, 45, 99};
-    Solution s;
-    cout << "Second largest: " << s.secondlargest(arr, arr.size()) << endl;
-    return 0;
-}
+
